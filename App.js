7b57b160c0f7dd50673abf797aa3d2303908127b
nayemislam33046 <nayemislam33046@gmail.com>
2024-11-15 21:40:56 +0600

    ecommerce

--- App.js
+++ App.js
@@ -3,14 +3,18 @@ const cors = require("cors");
 const Product = require("./router/Products");
 const singleProduct = require("./router/SingleProduct");
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true)
+  next()
+})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use("/", Product);
 app.use("/", singleProduct);
 module.exports = app;
